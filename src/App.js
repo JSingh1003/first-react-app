cb317e835b12fe4e525dda65cb90f0000f219667
Joginder Singh <singhjoginder1003@gmail.com>
2023-05-07 18:52:23 +0200

    Intro to JSX

--- src/App.js
+++ src/App.js
@@ -2,11 +2,26 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const firstName = "John";
+  const lastName = "Wick";
+  const age = 28;
+  const job = "Loving Husband";
+
+  const inputPlaceholder = "Enter Your Details";
+  const detailsInputBox = (
+    <input placeholder={inputPlaceholder} autoComplete></input>
+  );
+
+  const getFullName = (firstName, lastName) => {
+    return `${firstName} ${lastName}`;
+  };
   return (
     <div className="App">
-      <h3>Full Name: John Wick</h3>
-      <p>Age: 28</p>
-      <p>Job: Assassin</p>
+      <h3>Full Name: {getFullName(firstName, lastName)} </h3>
+      <p>Age: {age}</p>
+      <p>Job: {job}</p>
+
+      {detailsInputBox}
     </div>
   );
 }
